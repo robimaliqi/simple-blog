d1ec3b1a33cf5b84fb8da0190cb534f1ac8cf0c2
Robi <robi.maliqi@hotmail.co.uk>
2023-04-23 21:08:03 +0100

    used url helpers to add link to each article on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  get "/articles/:id", to: "articles#show", as: :article
   # Defines the root path route ("/")
   # root "articles#index"
 end
