df18920323cab04b822ace66b18f7a26fac34b1e
Robi <robi.maliqi@hotmail.co.uk>
2023-04-26 19:57:57 +0100

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root  "articles#index"
-  
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show", as: :article
   # Defines the root path route ("/")
   # root "articles#index"
+  root  "articles#index"
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show", as: :article
+  
 end
