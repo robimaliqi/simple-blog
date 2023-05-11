cf3727997731191dd3d78b1539fee0e7c2049a4f
Robi <robi.maliqi@hotmail.co.uk>
2023-05-11 20:48:13 +0100

    add create method to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,19 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(articles_params)
+    if articles.save
+    redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(article).permit(:title, :content)
+  end
 end
