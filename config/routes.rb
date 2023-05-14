5c145ad060ebd54b9974ef9155fcb136c55c5ebf
Robi <robi.maliqi@hotmail.co.uk>
2023-05-14 21:03:03 +0100

    added route for editing articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root  "articles#index"
+  get "/articles/:id/edit", to: "articles#edit"
   post "/articles", to: "articles#create"
   get "/articles/new", to: "articles#new"
   get "/articles", to: "articles#index"
